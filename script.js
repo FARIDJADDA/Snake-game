cbb5984401fb997d985f45e040beeadd39b9e9e6
Djadda Farid <faridjadda750@gmail.com>
2020-10-31 23:15:01 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 window.onload = function() {
-  var canvas = document.createElement('canvas');
+
+  var canvas;
+  var ctx;
+  var delay = 1000;
+  var xCoord = 0;
+  var yCoord = 0;
+
+  init();
+
+  function init() {
+      canvas = document.createElement('canvas');
       canvas.width = 900;
       canvas.height = 600;
       canvas.style.border = "1px solid";
       document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+    }
 
-      var ctx = canvas.getContext('2d');
-      ctx.fillStyle = "crimson"
-      ctx.fillRect(30, 30, 100, 50)
-}
+    function refreshCanvas() { 
+      xCoord += 2;
+      yCoord += 2;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "crimson";
+      ctx.fillRect(xCoord, yCoord, 100, 50);
+      setTimeout(refreshCanvas, delay);
+    }
+  }
