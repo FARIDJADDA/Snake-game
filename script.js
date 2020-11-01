ebb6baf7031312d7f8d0287ff2038ab1aaeb2ce1
Djadda Farid <faridjadda750@gmail.com>
2020-11-01 01:52:35 +0100

    snake function & Apple function & handleKeyDown function & setDirection function & advance function

--- script.js
+++ script.js
@@ -1,29 +1,135 @@
 window.onload = function() {
 
-  var canvas;
+  var canvasWidth = 900;
+  var canvasHeight = 600;
+  var blockSize = 30;
   var ctx;
-  var delay = 1000;
-  var xCoord = 0;
-  var yCoord = 0;
+  var delay = 100;
+  var snakee;
+  var applee;
+  var widthInBlocks = canvasWidth/blockSize;
+  var heightInBlocks = canvasHeight/blockSize;
 
   init();
 
   function init() {
-      canvas = document.createElement('canvas');
-      canvas.width = 900;
-      canvas.height = 600;
+      var canvas = document.createElement('canvas');
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
       canvas.style.border = "1px solid";
       document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6, 4],[5, 4], [4, 4] ], "right");
+        applee = new Apple([10,10]);
         refreshCanvas();
     }
 
-    function refreshCanvas() { 
-      xCoord += 2;
-      yCoord += 2;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "crimson";
-      ctx.fillRect(xCoord, yCoord, 100, 50);
+    function refreshCanvas() {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      snakee.draw();
+      snakee.advance();
+      applee.draw();
       setTimeout(refreshCanvas, delay);
     }
+    function drawBlock(ctx, position) {
+      var x = position[0] * blockSize;
+      var y = position[1] * blockSize;
+      ctx.fillRect(x,y,blockSize,blockSize)
+    }
+
+    function Snake(body, direction) {
+      this.body = body;
+      this.direction = direction;
+      this.draw = function() { 
+        ctx.save();
+        ctx.fillStyle = "#B06660";
+        for(var i = 0; i < this.body.length; i++) {
+          drawBlock(ctx, this.body[i]);
+        };
+        ctx.restore();
+    };
+    this.advance = () => {
+      var nextPosition = this.body[0].slice();
+      switch (this.direction) {
+        case "left":
+          nextPosition[0] -= 1;
+          break;
+        case "right":
+          nextPosition[0] += 1;
+          break;
+        case "down":
+          nextPosition[1] += 1;
+          break;
+        case "up":
+          nextPosition[1] -= 1;
+          break;
+      
+        default:
+          throw("invalide direction");
+      }
+      this.body.unshift(nextPosition)
+      this.body.pop();
+    };
+    this.setDirection = function(newDirection) {
+      var alloweDirections;
+      switch (this.direction) {
+        case "left":
+        case "right":
+          alloweDirections = ["up", "down"];
+          break;
+        case "down":
+        case "up":
+          alloweDirections = ["left", "right"];
+          break;
+        default: throw("invalide direction");
+      }
+      if(alloweDirections.indexOf(newDirection) > -1) {
+        this.direction = newDirection;
+      }
+    };
+    this.checkCollision = function() {
+      var wallCollision = false;
+      var snakeCollision = false;
+
+    }
   }
+
+  function Apple(position) {  
+    this.position = position;
+    this.draw = function() {
+      ctx.save();
+      ctx.fillStyle = "#FEABB9"
+      ctx.beginPath();
+      var radius = blockSize/2;
+      var x = position[0]* blockSize + radius;
+      var y = position[1]* blockSize + radius;
+      ctx.arc(x,y, radius, 0, Math.PI*2, true);
+      ctx.fill();
+      ctx.restore();
+    }
+  }
+
+
+  document.onkeydown = function handleKeyDown(e) {
+    var key = e.keyCode;
+    var newDirection;
+    switch (key) {
+      case 37:
+        newDirection = "left";
+        break;
+      case 38:
+        newDirection = "up";
+        break;
+      case 39:
+        newDirection = "right";
+        break;
+      case 40:
+        newDirection = "down";
+        break;
+    
+      default: return;
+    }
+    snakee.setDirection(newDirection);
+  }
+
+}
